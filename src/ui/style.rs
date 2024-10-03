dd6da36c79e3ee23035dbaf56d55ffa0ab504c13
Yanis <white.night.447@gmail.com>
2024-10-04 00:36:23 +0300

    Split main.rs to terminal and style files.
    # Conflicts:
    #	src/main.rs

---
+++ src/ui/style.rs
@@ -0,0 +1,9 @@
+use ratatui::style::palette::tailwind;
+use ratatui::style::Color;
+
+pub(crate) const APP_HEADER_BG: Color = tailwind::BLACK;
+pub(crate) const NORMAL_ROW_COLOR: Color = tailwind::SLATE.c950;
+pub(crate) const ALT_ROW_COLOR: Color = tailwind::SLATE.c900;
+pub(crate) const SELECTED_STYLE_FG: Color = tailwind::YELLOW.c700;
+pub(crate) const TEXT_COLOR: Color = tailwind::SLATE.c200;
+pub(crate) const NOT_AVAILABLE_TEXT_COLOR: Color = tailwind::RED.c500;
