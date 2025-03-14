74bbbfe66eff90b1388ad624e58560db7841c09a
Yanis Vasiliadis <white.night.447@gmail.com>
2025-03-14 18:26:03 +0300

    Update release shells script.

--- build_release.sh
+++ build_release.sh
@@ -5,5 +5,9 @@ cargo build --release
 VERSION=$(cargo pkgid | sed 's/.*#//')
 BINARY=ssh-manager-rust
 
-cp target/release/$BINARY target/release/${BINARY}-${VERSION}
-echo "Release ${BINARY}-${VERSION}"
+cp target/release/$BINARY ${BINARY}-${VERSION}
+echo "Linux release ${BINARY}-${VERSION}"
+
+cargo build --target aarch64-linux-android --release
+cp target/aarch64-linux-android/release/$BINARY ${BINARY}-${VERSION}-android
+echo "Android release ${BINARY}-${VERSION}"
